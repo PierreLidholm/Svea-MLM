00d991ee8ffecddfcc3481e847ae2baf32198f55
Pierre Lidholm <Pirrelidholm@gmail.com>
2024-12-11 11:50:04 +0100

    changed name of property

--- Svea-MLM/Controllers/MLMController.cs
+++ Svea-MLM/Controllers/MLMController.cs
@@ -8,23 +8,23 @@ namespace Svea_MLM.Controllers
   [ApiController]
   public class MLMController : Controller
   {
-      private ISalesmanService salesmanService;
+    private ISalesmanService salesmanService;
 
-      public MLMController(ISalesmanService salesmanService)
+    public MLMController(ISalesmanService salesmanService)
+    {
+      this.salesmanService = salesmanService;
+    }
+
+    [HttpGet("get")]
+    public ActionResult Get([FromQuery] int columns, [FromQuery] int rows, [FromQuery] int totalSimulations)
+    {
+      if (columns < 2 || rows < 2 || totalSimulations <= 0)
       {
-          this.salesmanService = salesmanService;
+        return BadRequest("Invalid columns length, row length or total simulations.");
       }
 
-      [HttpGet("get")]
-      public ActionResult Get([FromQuery] int columns, [FromQuery] int rows, [FromQuery] int totalRuns)
-      {
-          if (columns <= 2 || rows <= 2 || totalRuns <= 0)
-          {
-              return BadRequest("Invalid columns length, row length or total simulations.");
-          }
-
-          var result = salesmanService.SimulateMLM(columns, rows, totalRuns);
-          return Ok(result); 
-      }
+      var result = salesmanService.SimulateMLM(columns, rows, totalSimulations);
+      return Ok(result);
+    }
   }
 }
